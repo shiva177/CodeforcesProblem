9da90dab8c2e82f89aeb285c4adabc31f8ea6157
shiva177 <shivam.kr1729@gmail.com>
2025-01-01 16:09:38 +0530

    second commit

---
+++ A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+  ll t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    cout<<(-n+1)<<" "<<n<<endl;
+  }
+}
